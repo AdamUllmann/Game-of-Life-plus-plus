9e132ecdc57b2202f7fdba9b1b129025d95d9570
adamullmann <adamullmannbusiness@gmail.com>
2023-09-03 21:13:28 -0700

    Refactored code, added user input, zoom feature, simulation speed controls, and a quick way to randomize/clear the simulation grid.

--- game of life/game of life.cpp
+++ game of life/game of life.cpp
@@ -2,21 +2,34 @@
 // by Adam Ullmann
 
 #include "raylib.h"
-//#include <math.h>
 
-const int screenWidth = 1700;
-const int screenHeight = 900;
+// Constants
+const int screenWidth = 1280;
+const int screenHeight = 720;
 const int cellSize = 5;
 const int gridWidth = screenWidth / cellSize;
 const int gridHeight = screenHeight / cellSize;
 
+// Global Variables
 bool grid[gridWidth][gridHeight] = { 0 };
 bool gridBuffer[gridWidth][gridHeight] = { 0 };
+bool gamePaused = true;
+int zoomedCellSize = 5;
+int frameCounter = 0;
+int updateEveryNFrames = 8;
 
-void InitGrid() {
+void randomizeGrid() {
     for (int i = 0; i < gridWidth; i++) {
         for (int j = 0; j < gridHeight; j++) {
             grid[i][j] = (GetRandomValue(0, 1) == 1);
+        }
+    }
+}
+
+void clearGrid() {
+    for (int i = 0; i < gridWidth; i++) {
+        for (int j = 0; j < gridHeight; j++) {
+            grid[i][j] = 0;
         }
     }
 }
@@ -55,7 +68,6 @@ void UpdateGrid() {
             }
         }
     }
-
     // copy buffer back to the grid
     for (int i = 0; i < gridWidth; i++) {
         for (int j = 0; j < gridHeight; j++) {
@@ -64,34 +76,76 @@ void UpdateGrid() {
     }
 }
 
-void DrawGrid() {
+void DrawSim() {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
     for (int i = 0; i < gridWidth; i++) {
         for (int j = 0; j < gridHeight; j++) {
             if (grid[i][j]) {
-                DrawRectangle(i * cellSize, j * cellSize, cellSize, cellSize, WHITE);
+                DrawRectangle(i * zoomedCellSize, j * zoomedCellSize, zoomedCellSize, zoomedCellSize, WHITE);
             }
         }
     }
+    if (gamePaused) {
+        for (int i = 0; i <= gridWidth; i++) {
+            DrawLine(i * zoomedCellSize, 0, i * zoomedCellSize, screenHeight, { 35,35,35,255 });
+        }
+        for (int j = 0; j <= gridHeight; j++) {
+            DrawLine(0, j * zoomedCellSize, screenWidth, j * zoomedCellSize, { 35,35,35,255 });
+        }
+    }
+    EndDrawing();
 }
 
-int main()
-{
-    InitWindow(screenWidth, screenHeight, "Game of Life");
-    SetTargetFPS(30);
-
-    InitGrid();
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        UpdateGrid();
-        DrawGrid();
-
-        EndDrawing();
+void controls() {
+    if (gamePaused) {
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            int mouseX = GetMouseX() / zoomedCellSize;
+            int mouseY = GetMouseY() / zoomedCellSize;
+            grid[mouseX][mouseY] = true;
+        }
+        if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
+            int mouseX = GetMouseX() / zoomedCellSize;
+            int mouseY = GetMouseY() / zoomedCellSize;
+            grid[mouseX][mouseY] = false;
+        }
+    }
+    int mouseWheelMove = GetMouseWheelMove();
+    if (mouseWheelMove != 0) {
+        zoomedCellSize += mouseWheelMove;
+        if (zoomedCellSize < 5) zoomedCellSize = 5;
+        if (zoomedCellSize > 50) zoomedCellSize = 50;
+    }
+    if (IsKeyPressed(KEY_SPACE)) {
+        gamePaused = !gamePaused;
+    }
+    if (IsKeyPressed(KEY_UP) && updateEveryNFrames > 1) {   // frame control
+        updateEveryNFrames--; 
+    }
+    if (IsKeyPressed(KEY_DOWN)) {
+        updateEveryNFrames++;
     }
 
-    CloseWindow();
+    if (IsKeyPressed(KEY_R)) randomizeGrid();
+    if (IsKeyPressed(KEY_C)) clearGrid();
+}
 
+void runSim() {
+    while (!WindowShouldClose()) {
+        controls();
+           frameCounter++;
+        if (!gamePaused && frameCounter % updateEveryNFrames == 0) {
+            UpdateGrid();
+        }
+        DrawSim();
+    }
+}
+
+int main() {
+    InitWindow(screenWidth, screenHeight, "Game of Life");
+    SetTargetFPS(480);
+    runSim();
+    CloseWindow();
     return 0;
 }
